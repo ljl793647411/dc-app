a1272bcfafaae5b65092d4b1b16f01ed693b7020
ljl793647411 <lijinlong@yunquna.com>
2020-12-06 14:02:40 +0800

    feat:订单页

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,15 +1,88 @@
 <template>
-  <view>
-      这是订单页
-  </view>
+    <view class="order-tabbar-pages">
+        <u-tabs :list="tabsList" :current="current" @change="tabsChange" active-color="#E02020" inactive-color="#000000c9" :active-item-style="{color: '#000000'}"></u-tabs>
+        <view class="order-content">
+            <view v-for="(item, index) in list" :key="index">
+                <order-item :data="item"></order-item>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
+import OrderItem from './order-item'
 export default {
+    data() {
+        return {
+            // tab集合
+            tabsList: [
+                {
+                    name: '全部订单'
+                },
+                {
+                    name: '堂食'
+                },
+                {
+                    name: '预约'
+                },
+                {
+                    name: '外卖'
+                },
+                {
+                    name: '上门'
+                },
+            ],
+            current: 0,
+            list: [],
+            item: {
+                title: '醉徽湘（盛泰路店）',
+                date: '2020-12-01 07:21',
+                desc: '共计 8 件菜品',
+                type: 1,
+                status: 1
+            },
+        }
+    },
+    components: {
+       OrderItem, 
+    },
+    mounted() {
+        this.mock()
+    },
+    methods: {
+        // tab切换
+        tabsChange(index) {
+            this.current = index
+            this.mock()
+        },
+        aa() {
+            return Math.abs(Math.floor(Math.random() * 10))
+        },
+        mock() {
+            const length = this.aa()
+            const enums = [1, 2]
+            let _list = []
+            for(let i = 0; i <= length;i++) {
+                let _item = {...this.item}
+                _item.type = this.aa() % 2 === 1 ? 1 : 2
+                _item.status = this.aa() % 2 === 1 ? 1 : 2
+                _list.push(_item)
+            }
+            this.list = _list
+        }
+    }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .order-tabbar-pages {
+        width: 100%;
 
+        .order-content {
+            width: 100%;
+            padding: 16px;
+            background: #ffffff;
+        }
+    }
 </style>
---
+++ src/pages/order/order-item.vue
@@ -0,0 +1,127 @@
+<template>
+    <view>
+        <view class="order-item-box">
+            <view class="info-box">
+                <view class="top">
+                    <view class="title">{{data.title}}</view>
+                    <view class="status">{{orderStatusText(data)}}</view>
+                </view>
+                <view class="date">
+                    {{data.date}}
+                </view>
+                <view class="desc">
+                    {{data.desc}}
+                </view>
+            </view>
+            <view class="operation-box">
+                <view class="btn add" v-if="data.status == 1 && data.type == 1">加菜</view>
+                <view class="btn btn-2" v-if="data.status == 1 && data.type == 1">立即付款</view>
+                <view class="btn add" v-if="data.status == 2 && data.type == 2">再来一单</view>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Object,
+            default: {}
+        }
+    },
+    methods: {
+        // 获取订单状态文案
+        orderStatusText(item) {
+            if (item.status == 1 && item.type == 1) {
+                return '进行中'
+            }
+            if (item.status == 2 && item.type == 1) {
+                return '已完成'
+            }
+            if (item.status == 2 && item.type == 2) {
+                return '已送达'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .order-item-box {
+        width: 100%;
+        background: #FFFFFF;
+        box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.19);
+        border-radius: 8px;
+        padding: 13px 13px 13px 17px;
+        margin-bottom: 17px;
+
+        .operation-box {
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+
+            .btn {
+                margin-top: 13px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 28px;
+                border-radius: 21px;
+                font-size: 13px;
+                font-weight: 400;
+                line-height: 18px;
+                margin-left: 12px;
+            }
+
+            .add {
+                width: 85px;
+                background: #FFFFFF;
+                border: 1px solid #E02020;
+                color: #E02020;
+            }
+
+            .btn-2 {
+                width: 87px;
+                background: #FA6400;
+                color: #FFFFFF;
+            }
+        }
+        .info-box {
+            width: 100%;
+            .top {
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                .title {
+                    font-size: 18px;
+                    font-weight: 400;
+                    color: #1D1E2C;
+                    line-height: 24px;
+                }
+                .status {
+                    font-size: 14px;
+                    font-weight: 400;
+                    color: #F7B500;
+                    line-height: 24px;
+                }
+            }
+            .date {
+                font-size: 14px;
+                font-weight: 400;
+                color: rgba(0, 0, 0, 0.5);
+                line-height: 24px;
+            }
+            .desc {
+                margin-top: 3px;
+                font-size: 14px;
+                font-weight: 400;
+                color: #1D1E2C;
+                line-height: 24px;
+            }
+        }
+    }
+</style>
